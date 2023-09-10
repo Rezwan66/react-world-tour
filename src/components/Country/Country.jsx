8595d32d21aea6b0b42d8c06fbf4fd885ad60316
Rezwan66 <shaikhrezwan66@gmail.com>
2023-09-10 15:16:36 +0600

    added toggle state based on click with conditional rendering

--- src/components/Country/Country.jsx
+++ src/components/Country/Country.jsx
@@ -1,17 +1,32 @@
+import { useState } from "react";
 import "./Country.css";
 const Country = ({ country }) => {
-  console.log(country);
-  const { name, flags, population, area } = country;
-  return (
-    <div className="country">
-      <h3>Name: {name?.common}</h3>
-      <img src={flags?.png} alt="" />
-      <p>Population: {population}</p>
-      <p>
-        Area: {area} m<sup>2</sup>
-      </p>
-    </div>
-  );
+    console.log(country);
+    const { name, flags, population, area, cca3 } = country;
+
+    const [visited, setVisited] = useState(false);
+
+    const handleVisited = () => {
+        setVisited(!visited);
+    };
+
+    return (
+        <div className="country">
+            <h3>Name: {name?.common}</h3>
+            <img src={flags?.png} alt="" style={{ maxHeight: "200px" }} />
+            <p>Population: {population}</p>
+            <p>
+                Area: {area} m<sup>2</sup>
+            </p>
+            <p>
+                <small>Code: {cca3}</small>
+            </p>
+            <button onClick={handleVisited}>
+                {visited ? "Visited" : "Going"}
+            </button>
+            {visited && " I have visited this country."}
+        </div>
+    );
 };
 
 export default Country;
